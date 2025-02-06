ca71fae3b0cdabd5f4a748e8956392688a58951e
Clint James <cjestal@gmail.com>
2025-02-06 18:22:07 -0500

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Job Platform API')
+    .setDescription('These are the endpoints for the job platform app for AI Made in Africa')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory());
+
+  await app.listen(3001);
 }
 bootstrap();
