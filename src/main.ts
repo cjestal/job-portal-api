caa9bf1c2960c18475ef1f336398b7adf888361b
Clint James <cjestal@gmail.com>
2025-02-07 16:25:00 -0500

    updated port logic

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.PORT || 3001;
   const config = new DocumentBuilder()
     .setTitle('Job Platform API')
     .setDescription('These are the endpoints for the job platform app for AI Made in Africa')
@@ -12,7 +12,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory());
-
-  await app.listen(3001);
+  
+  await app.listen(port);
 }
 bootstrap();
