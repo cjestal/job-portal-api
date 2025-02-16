14fc3d4a3c718a12892bd71ab6d7fa7a6caebaca
Clint James <cjestal@gmail.com>
2025-02-15 23:16:29 -0500

    auth api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./endpoints/users');
 const companiesRouter = require('./endpoints/companies');
 const jobsRouter = require('./endpoints/jobs');
 const jobApplicationsRouter = require('./endpoints/job-applications');
+const authRouter = require('./endpoints/auth');
 
 const app = new Koa();
 
@@ -23,6 +24,7 @@ app.use(usersRouter.routes()).use(usersRouter.allowedMethods());
 app.use(companiesRouter.routes()).use(companiesRouter.allowedMethods());
 app.use(jobsRouter.routes()).use(jobsRouter.allowedMethods());
 app.use(jobApplicationsRouter.routes()).use(jobApplicationsRouter.allowedMethods());
+app.use(authRouter.routes()).use(authRouter.allowedMethods());
 
 const port = 3000;
 app.listen(port, () => {
